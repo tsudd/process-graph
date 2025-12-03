5f8a1cf4bb8a13f004673c158902a163c099e7e3
Aliaksandr Khrystsenka <alex.kr1stehko@gmail.com>
2025-12-03 21:28:46 +0300

    upd: repos base structure

---
+++ src/server/ProcessGraph.Domain/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,6 @@
+﻿namespace ProcessGraph.Domain;
+
+public interface IUnitOfWork
+{
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
--- src/server/ProcessGraph.Domain/Processes/IProcessRepository.cs
+++ src/server/ProcessGraph.Domain/Processes/IProcessRepository.cs
@@ -1,6 +1,14 @@
+using System.Collections.Immutable;
+using FluentResults;
+
 namespace ProcessGraph.Domain.Processes;
 
 public interface IProcessRepository
 {
-    
+    Task<Result<Process>> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<Result> CreateAsync(Process process, CancellationToken cancellationToken = default);
+    Task<Result> UpdateAsync(Process process, CancellationToken cancellationToken = default);
+    Task<Result<IImmutableList<Process>>> GetAllAsync(
+        CancellationToken cancellationToken = default
+    );
 }
